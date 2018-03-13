21bcf93cc8b337111151618b0a63623b2756b010
eric-elem <eric.elem@andela.com>
2018-03-13 09:25:57 +0300

    ch(dashboard): Ensure all checks data is sent
    - Ensure checks payload contains all fields
    - Ensure dashboard tests pass
    Delivers [#155514650]

--- application/static/js/dashboard.js
+++ application/static/js/dashboard.js
@@ -1,76 +1,186 @@
-$('#submitForms').click(function () {
-    var data = [];
-    $('.form').each(function (index, value) {
-        var form = $(this),
-            check = {};
-        form.find('[name]').each(function (index1, value1) {
-            if (index == 0) {
-                return;
+const requestHeaderTemplate = `
+    <div class="form-inline">
+        <input name="headerKey" type="text" class="form-control" placeholder="KEY">
+        <input name="headerValue" type="text" class="form-control" placeholder="VALUE">
+        <span class="removeHeader">&times;</span>
+    </div>
+`,
+    checkAssertionTemplate = `
+    <div class="assertion">
+        <select name="assertionSource" class="form-control assertion-value">
+            <option>Status Code</option>
+            <option>Response Time (ms)</option>
+        </select>
+        <select name="assertionComparison" class="form-control assertion-value">
+            <option>equal (number)</option>
+            <option>less than</option>
+            <option>less than or equal to</option>
+            <option>greater than</option>
+            <option>greater than or equal to</option>
+        </select>
+        <input name="assertionTargetValue" type="text" class="form-control assertion-value" id="targetValue" placeholder="Enter value">
+        <span class="remove-assertion">&times;</span>
+    </div>
+`,
+    checkTemplate = `
+    <div class="check" id="checkTemplate">
+        <span class="removeCheck">
+            <i class="fas fa-times"></i>
+        </span>
+        <nav class="nav" role="tablist">
+            <a class="nav-link active" data-toggle="tab" href="#request" role="tab">Request</a>
+            <a class="nav-link" data-toggle="tab" href="#assertions" role="tab">Assertions</a>
+        </nav>
+        <form class="form">
+            <div class="tab-content">
+                <div class="tab-pane active" id="request" role="tabpanel">
+                    <span class="tab-pane-heading">ENDPOINT</span>
+                    <div class="form-inline requestForm">
+                        <select name="method" class="form-control">
+                            <option>GET</option>
+                            <option>POST</option>
+                            <option>PUT</option>
+                            <option>DELETE</option>
+                        </select>
+                        <input name="url" type="text" class="form-control url-input" placeholder="URL">
+                    </div>
+                    <span class="tab-pane-heading">HEADERS</span>
+                    <div class="headers" id="headers"></div>
+                    <span class="addHeader">+ Add header</span>
+                </div>
+                <div class="tab-pane" id="assertions" role="tabpanel">
+                    <div class="assertions" id="assertionsBox">
+                        <div class="headings">
+                            <span>SOURCE</span>
+                            <span>COMPARISON</span>
+                            <span>TARGET VALUE</span>
+                        </div>
+                    </div>
+                    <span class="addAssertion">+ Add assertion</span>
+                </div>
+            </div>
+        </form>
+    </div>
+`;
+
+var checkId = 0;
+function addElement(event) {
+    var element = $($.trim(event.data.template));
+    var removeButton = element.children("[class^=remove]");
+    removeButton.on('click', { element: removeButton }, removeParent);
+    if (event.data.template == checkTemplate) {
+        var navLinks = element.find('.nav-link');
+        $(navLinks[0]).attr('href', `#request${checkId}`);
+        $(navLinks[1]).attr('href', `#assertions${checkId}`);
+        var tabPanes = element.find('.tab-pane');
+        $(tabPanes[0]).attr('id', `request${checkId}`);
+        $(tabPanes[1]).attr('id', `assertions${checkId}`);
+        var headersContainer = element.find('#headers');
+        element.find('.addHeader').on(
+            'click',
+            {
+                container: headersContainer,
+                template: requestHeaderTemplate
+            },
+            addElement
+        )
+
+        var assertionsContainer = element.find('#assertionsBox');
+        var assertionsEvent = {
+            data: {
+                container: assertionsContainer,
+                template: checkAssertionTemplate
             }
-            var field = $(this),
-                name = field.attr('name'),
-                value = field.val();
-            if (name in check) {
-                if (check[name].constructor === Array) {
-                    check[name].push(value);
-                } else {
-                    const tempValue = check[name];
-                    check[name] = [tempValue, value];
-                }
-            } else {
-                check[name] = value;
-            }
-        });
-        data.push(check);
-    });
-    $.ajax({
-        url: 'http://127.0.0.1:5000/dashboard',
-        type: 'POST',
-        data: JSON.stringify(data),
-        contentType: "application/json; charset=utf-8",
-        success: function (response) {
-            console.log("here", response);
+        };
+        addElement(assertionsEvent);
+        element.find('.addAssertion').on(
+            'click',
+            assertionsEvent.data,
+            addElement
+        )
+        checkId++;
+    }
+    event.data.container.append(element);
+}
+
+function removeParent(event) {
+    event.data.element.parent().remove();
+}
+
+function getFormData(form) {
+
+    var form = form,
+        data = {};
+
+    const list_entries = {
+        headerKeys: [],
+        headerValues: [],
+        assertionSources: [],
+        assertionComparisons: [],
+        assertionTargetValues: []
+    };
+
+    form.find('[name]').each(function (index1, value1) {
+        var field = $(this),
+            name = field.attr('name'),
+            value = field.val(),
+            plural_name = name + 's';
+
+        if (plural_name in list_entries) {
+            list_entries[plural_name].push(value);
+        } else {
+            data[name] = value;
         }
     });
+
+    $.extend(data, list_entries);
+    return data
+
+}
+
+function postData(data, callback) {
+    if (data.constructor === Array && data.length > 0) {
+        $.ajax({
+            url: '/dashboard',
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            success: callback
+        });
+    } else {
+        return false;
+    }
+}
+
+function onPostData() {
+
+}
+
+$('#addCheck').on(
+    'click',
+    {
+        container: $('#checksFormContainer'),
+        template: checkTemplate
+    },
+    addElement
+);
+
+$('#submitForms').on('click', function() {
+    var data = [];
+    $('.form').each(function() {
+        data.push(getFormData($(this)));
+    });
+    postData(data, onPostData)
 });
 
-var headerIndex = 0;
-var assertionIndex = 1;
-var checkIndex = 0;
-$("#addCheck").click(function () {
-    var clone = $("#checkTemplate").clone().attr('id', 'check' + checkIndex);
-    const tabPanes = clone.children(".form").children(".tab-content").children(".tab-pane");
-    const requestPane = $(tabPanes[0]);
-    const assertionsPane = $(tabPanes[1]);
-    requestPane.attr('id', 'request' + checkIndex);
-    var headersBox = requestPane.children('#headers');
-    headersBox.attr('id', 'headers' + checkIndex);
-    const requestForm = requestPane.children('.requestForm');
-    requestPane.children(".add-button").click(function () {
-        var clone = $("#header").clone().attr('id', 'header' + headerIndex);
-        clone.children('.remove-header').click(function () {
-            $(this).parent().remove();
-        });
-        clone.appendTo(headersBox);
-        headerIndex++;
-    });
-    assertionsPane.attr('id', 'assertions' + checkIndex);
-    var assertionsBox = assertionsPane.children('#assertions-box');
-    assertionsBox.attr('id', 'assertions' + checkIndex);
-    assertionsPane.children(".add-button").click(function () {
-        var clone = $("#assertion0").clone().attr('id', 'assertion' + assertionIndex);
-        clone.children('.remove-assertion').click(function () {
-            $(this).parent().remove();
-        });
-        clone.appendTo(assertionsBox);
-        assertionIndex++;
-    });
-    const navTabs = clone.children(".nav").children(".nav-link");
-    $(navTabs[0]).attr('href', '#request' + checkIndex);
-    $(navTabs[1]).attr('href', '#assertions' + checkIndex);
-    clone.children('.removeCheck').click(function () {
-        $(this).parent().remove();
-    });
-    clone.appendTo("#checksFormContainer");
-    checkIndex++;
-});
+if(typeof module !== 'undefined') {
+    module.exports.main = {
+        requestHeaderTemplate,
+        checkAssertionTemplate,
+        checkTemplate,
+        getFormData,
+        addElement,
+        removeParent,
+        postData
+    }
+}
